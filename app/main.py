60b98c3e5ed3082ccb8ebc8d5d2eb7e243a7bd00
Dilusha Amarasekara <60927547+DilushaAmarasekara@users.noreply.github.com>
2022-04-27 16:21:31 +0530

    MFD-02

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI
+import pathlib
+from tempfile import template
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
+BASE_DIR = pathlib.Path(__file__).parent
+print((BASE_DIR / "templates").exists())
 app = FastAPI()
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
-@app.get("/") # http get
-def home_view():
-    return {"hello":"world"}
+@app.get("/",response_class=HTMLResponse) # http get
+def home_view(request:Request):
+    return templates.TemplateResponse("home.html",{"request":request, "abc":123})
 
 @app.post("/") # http post
 def home_detail_view():
