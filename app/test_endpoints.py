0690c5b9361e7eb571a9f4a6a88dcc1ce4c800d1
Dilusha Amarasekara <60927547+DilushaAmarasekara@users.noreply.github.com>
2022-04-27 19:00:45 +0530

    MFD-04 fastApi Git & pre-commit

--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -8,9 +8,9 @@ def test_get_home():
     assert response.text != "<h1>Hello world</h1>"
     assert response.status_code == 200
     assert "text/html" in response.headers['content-type']
-    
+
 def test_post_home():
     response = client.post("/") # request.post("") python request
     assert response.status_code == 200
     assert "application/json" in response.headers['content-type']
-    assert response.json() == {"hello":"world"}    
+    assert response.json() == {"hello":"world"}
