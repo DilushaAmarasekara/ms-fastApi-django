646417c8a3a815e5ba8f8cb29327d22532c97313
Dilusha Amarasekara <60927547+DilushaAmarasekara@users.noreply.github.com>
2022-04-28 11:58:51 +0530

    MFD-08

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 import pathlib
+import io
+import uuid
 from tempfile import template
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, File, UploadFile
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 
 BASE_DIR = pathlib.Path(__file__).parent
+UPLOAD_DIR = BASE_DIR / "uploaded"
 print((BASE_DIR / "templates").exists())
 app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
@@ -16,3 +19,13 @@ def home_view(request:Request):
 @app.post("/") # http post
 def home_detail_view():
     return {"hello":"world"}
+
+@app.post("/img-echo/", response_class=FileResponse) # http post
+async def img_echo_view(file:UploadFile = File(...)):
+    bytes_str = io.BytesIO(await file.read())
+    fname = pathlib.Path(file.filename)
+    fest = fname.suffix # .jpg, .txt
+    dest = UPLOAD_DIR / f"{uuid.uuid1()}{fest}"
+    with open(str(dest),'wb') as out:
+        out.write(bytes_str.read())
+    return dest
